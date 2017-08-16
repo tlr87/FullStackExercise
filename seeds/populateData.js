61f79689f8f2bd9c4aa693a5a19d8ffde0a226cc
Tom Revill <tom.revill@gmail.com>
2017-08-16 12:01:36 +1200

    cleaning up greetings

--- seeds/populateData.js
+++ seeds/populateData.js
@@ -1,13 +1,12 @@
-
 exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
-  return knex('greetings').del()
+  return knex('DataTable').del()
     .then(function () {
       // Inserts seed entries
-      return knex('greetings').insert([
-        {id: 1, text: 'Hello There'},
-        {id: 2, text: 'Greetings Friend'},
-        {id: 3, text: 'Well Met'}
+      return knex('DataTable').insert([
+        {id: 1, text: 'Joe', numbers:20},
+        {id: 2, text: 'Ted',numbers:30},
+        {id: 3, text: 'Bob',numbers:50}
       ]);
     });
 };
